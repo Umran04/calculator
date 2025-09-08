b7b8053c7c82daa45dd6fa458f98971ae858866d
Umran <mohammedumran.0416@gmail.com>
2025-09-08 18:30:31 +0100

    added the buttons i will use and made the skeleton, i think the visual elements are almost done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const div = document.getElementById('calculator')
+const div = document.getElementById('grid')
+
+let rowOne = ['7','8','9','/']
+let rowTwo = ['4','5','6','X']
+let rowThree = ['1','2','3','-']
+let rowFour = ['.','0','=','+']
+
+function createBtn(array){
+
+    for(let i = 0; i <array.length; i++){
+        const button = document.createElement('button')
+        button.classList.add('button')
+        button.textContent = array[i]
+        div.appendChild(button)
+    }
+
+    
+
+}
+
+createBtn(rowOne)
+createBtn(rowTwo)
+createBtn(rowThree)
+createBtn(rowFour)
+
